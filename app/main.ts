338e1be090b15584d795f339d67c83d24d797f9d
jandresjarazo-rbi <jandresjarazo@rbi.com>
2025-07-20 13:37:58 -0300

    First attempt at the parser

--- app/main.ts
+++ app/main.ts
@@ -3,13 +3,53 @@ import * as net from "net";
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
+function parseRESP(buffer: Buffer ): string[] {
+  const bufferParsed = buffer.toString();
+  const lines = bufferParsed.split("\r\n");
+
+  if (lines.length && lines[0].startsWith("*")) {
+    throw new Error("Expecting lines without starting line, or RESP array");
+  }
+
+  const res: string[] = [];
+
+  for (let i = 1; i < lines.length; i++) {
+    const line = lines[i];
+
+    if (line.startsWith("$")) {
+      const len = parseInt(line.slice(1), 10);
+      const value = lines[i + 1];
+      res.push(len === -1 ? null as unknown as string : value);
+      i++;
+    }
+  }
+
+
+  return res;
+}
+
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
   // Handle connection
   connection.on("data", (chunk) => {
     console.log("Received data: ", chunk.toLocaleString());
 
-    connection.write('+PONG\r\n');
+    try {
+      const commandParts = parseRESP(chunk);
+      const command = commandParts[0].toUpperCase();
+
+      if (command === "PING") {
+        connection.write("+PONG\r\n");
+      } else if (command === "ECHO") {
+        const message = commandParts[1] ?? "";
+        connection.write(`$${message.length}\r\n${message}\r\n`);
+      } else {
+        connection.write("-ERR unknown command\r\n");
+      }
+    } catch (e) {
+      console.error("Parsing error:", e);
+      connection.write("-ERR parsing failed\r\n");
+    }
   });
 
   connection.on("end", () => {
